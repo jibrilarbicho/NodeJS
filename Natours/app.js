1536db68d54a680a0d38c6979a385aae39a840e6
Jibril Arbicho <jibrilarbicho2@gmail.com>
2023-11-09 09:58:52 +0300

    mounting and creating multiple routes

--- Natours/app.js
+++ Natours/app.js
@@ -49,11 +49,63 @@ const updateTour = (req, res) => {
     data: { tour: '<Updated Tour here>' },
   });
 };
+const deleteTour = (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
+
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'this route is not defined',
+  });
+};
+
+const getAllUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'this route is not defined',
+  });
+};
+const createUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'this route is not defined',
+  });
+};
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'this route is not defined',
+  });
+};
+
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'this route is not defined',
+  });
+};
 // app.get('/api/v1/tours/:id', getTour);
 // app.patch('/api/v1/tours/:id', updateTour);
 // app.post('/api/v1/tours', createTour);
-app.route('api/v1/tours/:id').get(getTour).post(createTour);
-app.route('/api/v1/tours').patch(updateTour);
+
+const tourRouter = express.Router();
+const userRouter = express.Router();
+tourRouter.route('/:id').get(getTour).post(createTour);
+tourRouter.route('/').get(getTour).patch(updateTour).delete(deleteTour);
+userRouter.route('/').get(getAllUsers).post(createUsers);
+userRouter.route('/:id').get(getAllUser).patch(updateUser).delete(deleteUser);
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on the port ${port}`);
---
+++ Natours/server.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const dotenv = require('dotenv');
+const app = require('.app');
+dotenv.config({ path: './config.env' });
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.PASSWORD);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log('DB connection succesfully');
+  });
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`app running on port ${port}...`);
+});
