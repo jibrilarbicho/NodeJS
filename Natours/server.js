05e88e1dbaa6debc9f17ce8103396c6524dc96ec
Jibril Arbicho <jibrilarbicho2@gmail.com>
2023-11-09 22:22:23 +0300

    creating simple tour model

--- Natours/server.js
+++ Natours/server.js
@@ -1,27 +1,45 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const app = require('./app');
-const port = 3000;
 
-app.listen(port, () => {
-  console.log(`App running on the port ${port}`);
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log('DB connection successfully');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour Must have a price'],
+  },
 });
-
-// const mongoose = require('mongoose');
-
-// const dotenv = require('dotenv');
-// const app = require('.app');
-// dotenv.config({ path: './config.env' });
-// const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.PASSWORD);
-// mongoose
-//   .connect(DB, {
-//     useNewUrlParser: true,
-//     useCreateIndex: true,
-//     useFindAndModify: false,
-//   })
-//   .then((con) => {
-//     console.log(con.connections);
-//     console.log('DB connection succesfully');
-//   });
-// const port = process.env.PORT || 3000;
-// app.listen(port, () => {
-//   console.log(`app running on port ${port}...`);
-// });
+const Tour = mongoose.mongose.model('Tour', tourSchema);
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
