f1126b484a04129a20968bf78028511eb0284996
Jibril Arbicho <jibrilarbicho2@gmail.com>
2023-11-16 15:24:51 +0300

    Better Error handling Reafctoring

--- Natours/app.js
+++ Natours/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const Errohndler = require('./Controller/controllerError');
 const morgan = require('morgan');
 const tourRouter = require('./routes/tourRouter');
 const userRouter = require('./routes/userRouter');
@@ -24,12 +25,5 @@ app.all('*', (req, res, next) => {
 });
 //Error Handling Middleware (app.use((err, req, res, next) => { ... })):
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    messsage: err.message || 'error',
-  });
-});
+app.use(Errohndler);
 module.exports = app;
