6d97b52fd10d5041a17baf6e14e605c676494c4f
Jibril Arbicho <jibrilarbicho2@gmail.com>
2023-11-16 15:04:19 +0300

    Implementing a Global Error handling Middleware

--- Natours/app.js
+++ Natours/app.js
@@ -10,10 +10,26 @@ app.use(morgan('dev'));
 ``;
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+// app.all('*', (req, res, next) => {
+//   res.status(404).json({
+//     status: 'faill',
+//     messsage: `can't find ${req.originalUrl}`,
+//   });
+// });
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'faill',
-    messsage: `can't find ${req.originalUrl}`,
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.status = 'fail';
+  err.statusCode = 404;
+  next(err);
+});
+//Error Handling Middleware (app.use((err, req, res, next) => { ... })):
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  res.status(err.statusCode).json({
+    status: err.status,
+    messsage: err.message || 'error',
   });
 });
 module.exports = app;
