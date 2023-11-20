d016ad051fa12f1738c3801e9cd42868d5c3b8ad
Jibril Arbicho <jibrilarbicho2@gmail.com>
2023-11-20 20:18:40 +0300

    signing users

--- Natours/Controller/authController.js
+++ Natours/Controller/authController.js
@@ -1,10 +1,15 @@
 const User = require('./../Models/userModel');
 const catchAsync = require('./../utils/catchAsync');
-
+const jwt = require('jsonwebtoken');
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create(req.body);
+  const token = jwt.sign({ id: newUser.id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRESIN,
+  });
+
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
