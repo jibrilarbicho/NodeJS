2a7caf6e1c3c28e2d862e6fe0febe5c9f6a0168c
Jibril Arbicho <jibrilarbicho2@gmail.com>
2023-11-21 17:35:12 +0300

    protecting tour routes part2

--- Natours/Controller/authController.js
+++ Natours/Controller/authController.js
@@ -1,7 +1,9 @@
 const User = require('./../Models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
+const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
+const { verify } = require('crypto');
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     // expiresIn: process.env.JWT_EXPIRES_IN,
@@ -32,7 +34,7 @@ exports.login = catchAsync(async (req, res, next) => {
   const token = signToken(user._id);
 
   const correct = await user.correctPassword(password, user.password);
-  if (!user || correct) {
+  if (!user || !correct) {
     return next(new AppError('Incorrect email or password', 401));
   }
   res.status(200).json({
@@ -51,11 +53,29 @@ exports.protect = catchAsync(async (req, res, next) => {
   ) {
     token = req.headers.authorization.split(' ')[1];
   }
-  console.log(token);
+  // console.log(token);
   if (!token) {
     return next(
       new AppError('You are not logged in plese login to get  accesss', 401)
     );
   }
+  //Verification token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+  // console.log(decoded);
+  const freshUser = await User.findById(decoded.id);
+  if (!freshUser) {
+    return next(
+      new AppError('The user belonging to this token no longer exist ', 401)
+    );
+  }
+  if (freshUser.changedPasswordAfter(decoded.iat)) {
+    return next(
+      new AppError(
+        'the user recently changed the password please login again ',
+        401
+      )
+    );
+  }
+  req.user = freshUser;
   next();
 });
--- Natours/Models/userModel.js
+++ Natours/Models/userModel.js
@@ -31,6 +31,7 @@ const userSchema = new mongoose.Schema({
       message: 'Passwords are not the same',
     },
   },
+  paswordChangedAt: Date,
 });
 
 userSchema.pre('save', async function (next) {
@@ -44,6 +45,19 @@ userSchema.methods.correctPassword = async function (
   userPassword
 ) {
   return await bcrypt.compare(candidatePassword, userPassword);
+};
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+  if (this.paswordChangedAt) {
+    console.log(this.paswordChangedAt);
+    console.log('jimma');
+    const changedTimestamp = parseInt(
+      this.paswordChangedAt.getTime() / 1000,
+      10
+    ); //JWTTimestamp is in second while this.paswordChangedAt in ms
+    console.log(changedTimestamp, JWTTimestamp);
+    return JWTTimestamp < changedTimestamp;
+  }
+  return false;
 };
 const User = mongoose.model('User', userSchema);
 module.exports = User;
