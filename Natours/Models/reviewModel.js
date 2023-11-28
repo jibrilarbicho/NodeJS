f5b818f377c7fe47181a2598e3f8f87853dd0cb2
Jibril Arbicho <jibrilarbicho2@gmail.com>
2023-11-28 15:49:57 +0300

    Calculating Average Ratings On Tours part-2

--- Natours/Models/reviewModel.js
+++ Natours/Models/reviewModel.js
@@ -32,7 +32,28 @@ const reviewSchema = new mongoose.Schema(
     to0bject: { virtuals: true },
   }
 );
+// reviewSchema.statics.calcAverageRatings = async function (tourID) {
+//   const stats = await this.aggregate([
+//     {
+//       $match: { tour: tourID },
+//     },
+//     {
+//       $group: {
+//         _id: '$tour',
+//         nRating: { $sum: 1 },
+//         avgRating: { $avg: '$rating' },
+//       },
+//     },
+//   ]);
+//   console.log(stats);
+//   await Tour.findByIdAndUpdate(tourID, {
+//     ratingsQuantity: stats[0].nRating,
+//     ratingsAverage: stats[0].avgRating,
+//   });
+// };
 reviewSchema.statics.calcAverageRatings = async function (tourID) {
+  console.log('Received tourID:', tourID);
+
   const stats = await this.aggregate([
     {
       $match: { tour: tourID },
@@ -45,25 +66,74 @@ reviewSchema.statics.calcAverageRatings = async function (tourID) {
       },
     },
   ]);
-  console.log(stats);
-  await Tour.findByIdAndUpdate(tourID, {
-    ratingsQuantity: stats[0].nRating,
-    ratingsAverage: stats[0].avgRating,
-  });
+
+  if (stats.length > 0) {
+    console.log(stats);
+    await Tour.findByIdAndUpdate(tourID, {
+      ratingsQuantity: stats[0].nRating,
+      ratingsAverage: stats[0].avgRating,
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourID, {
+      ratingsQuantity: 0,
+      ratingsAverage: 4.5,
+    });
+  }
 };
+
 reviewSchema.post('save', function () {
   // this points to current review
   this.constructor.calcAverageRatings(this.tour);
 });
 reviewSchema.pre(/^find/, function (next) {
   this.populate({
-    path: 'tour', //tour Field in document
-    select: 'name',
-  }).populate({
     path: 'user',
     select: 'name photo',
   });
   next();
 });
+// reviewSchema.pre(/^find/, function (next) {
+//   this.populate({
+//     path: 'tour', //tour Field in document
+//     select: 'name',
+//   }).populate({
+//     path: 'user',
+//     select: 'name photo',
+//   });
+//   next();
+// });
+
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  console.log(this.getQuery);
+  this._currentQuery = await this.model.findOne(this.getQuery()); // Store the document in _currentQuery
+  console.log(this._currentQuery);
+  next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function () {
+  if (this._currentQuery) {
+    const { tour, constructor } = this._currentQuery;
+    await constructor.calcAverageRatings(tour);
+    console.log(this._currentQuery);
+  }
+});
+
 const Review = mongoose.model('Review', reviewSchema);
 module.exports = Review;
+// reviewSchema.pre(/^findOneAnd/);findbyIdAndUpdate and findByIdAndDelete
+
+// findByIdAndUpdate
+// findByIdAndDelete
+
+// reviewSchema.pre(/^findOneAnd/, async function (next) {
+//   this.r = await this.model.findOne(this.getQuery());
+//   // this.r = await this.findOne();
+//   console.log(this.r);
+//   next();
+// });
+// reviewSchema.post(/^findOneAnd/, async function () {
+//   // this.r = await this.findOne(); Does not work b/c the query alredy excuted
+
+//   await this.r.constructor.calcAverageRatings(this.r.tour);
+//   console.log(this);
+// });
