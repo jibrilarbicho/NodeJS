31aaa2a7fddba1bdb704ccb8a824d768b9b6a4a8
Jibril Arbicho <jibrilarbicho2@gmail.com>
2024-01-08 00:06:39 +0300

    Image Uploads Using Multer Users

--- Natours/Controller/userController.js
+++ Natours/Controller/userController.js
@@ -33,6 +33,9 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
   });
 });
 exports.updateMe = catchAsync(async (req, res, next) => {
+  console.log('files');
+  console.log(req.file);
+  console.log(req.body);
   // 1) Create error if user POSTs password data
   if (req.body.password || req.body.passwordConfirm) {
     return next(
